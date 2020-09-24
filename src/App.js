a524d32f206df4f7665c996753fd99d501cd9b15
Raghib Ahsan <40718368+Raghib502@users.noreply.github.com>
2020-09-24 19:32:25 +0600

    fully not complete

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
   const [loggedInUser, setLoggedInUser] = useState({});
   return (
     <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
-      <h3>Email :{loggedInUser.email}</h3>
       <Router>
         <Header/>
         <Switch>
